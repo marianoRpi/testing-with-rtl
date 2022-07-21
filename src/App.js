d061455bac52d9015452df3c96a47a0cb919a121
marianoRpi <mmacri@soyhenry.com>
2022-07-20 23:01:00 -0300

    Add simple button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
+import MyButton from './MyButton'
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Testing React components with RTL</h1>
+			<section>
+				<MyButton text='ima button!' />
+			</section>
 		</div>
 	)
 }
